cefe2c7393b039a6120757da49d26576d826a8cf
Iurii_Lepesevich <iurii.lepesevich@gmail.com>
2022-03-24 07:57:54 +0200

    Finished work 7

---
+++ Reports/Work_7.md
@@ -0,0 +1,56 @@
+# Task 1 (histogram)
+
+
+## Code:
+![](../Images/Work_7_Code.PNG)
+I have implemented histogram for my Y values of the dataset. It shows occurrences of different values in a variety of ranges.
+
+## Output:
+
+![](../Images/Work_7_Output.PNG)
+![](../Images/Work_7_Output_2.PNG)
+
+On the output, we can see my histogram on the first picture and histogram of this dataset from the kaggle resource. It is clearly seen that those correnspond a lot.
+
+
+
+
+
+
+
+# Task 2 (Theoretical part)
+
+
+## Explanation:
+No code to show. I have prepared theoretical material from different books, written down some citations and I am going to combine them in final version of my first part of the thesis.
+
+
+
+
+
+
+
+
+## Questions:
+
+### Task 1 (Histograms)
+
+More or less I managed to plot different data from my dataset (Y values, X values, whole dataset), however I have a really stupid question. If we are talking about Y values - I clearly understand the idea of histogram. As I already shown the program outputs histogram with occurrences of different values in a variety of ranges, which I think is what was asked.
+But, when I need to plot the whole dataset - I don't clearly understand, what should be plotted. I can assume that I would need to combine values of different parameters with the bars of their names, but what would be the values of the column? Would it be the maximum, or the average, or the sum of this parameter?
+
+
+
+
+### Task 2 (Embedding)
+
+Frankly speaking, the only experience I had with including categorical inputs was with dataset from your example:
+![](../Images/Work_7_Emb_1.PNG)
+and this model:
+![](../Images/Work_7_Emb_2.PNG)
+I can manage with constructing the model, however creation of the dataset that was in your example is kind of complicated. I don't really know how to create labels and X_classes with my dataset. In your dataset it was realized on the 39th line of code, but when I try to load data from my csv to 3 different variables (as it is done in your example) - there is an error (due to different loading technics). And I don't really know how to avoid this error.
+
+
+### Task 3 (Hyper-parameter inputs using argparse)
+
+
+For now it is the hardest task I was doing. I am really trying to understand the idea and technic of how argparse should be implemented, however I have no experience in it and the resources that I found and that were given are kind of not still enough. I wanted to ask, if you could recommend more resources (maybe you have workshops on YouTube doing something connected with it), or you could show me some examples of how it should work. I am sorry to upload the work with totally unfinished task.
---
+++ Work7/Regression_Task_Dataset4.py
@@ -0,0 +1,190 @@
+import pandas as pd
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+import torch.utils.data
+import torch.nn.functional as F
+
+
+
+plt.rcParams["figure.figsize"] = (12, 14) # size of window
+plt.style.use('dark_background')
+
+LEARNING_RATE = 1e-3
+BATCH_SIZE = 16
+TRAIN_TEST_SPLIT = 0.5
+
+
+def O_fun(x):
+    return np.sqrt( np.sum( np.power( (x - np.mean(x, axis=0)), 2 ), axis=0 ) / len(x) )
+
+def stand(x):
+    return ( x - np.mean(x, axis=0) ) / (O_fun(x) + 1e-8)
+
+
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self):
+        super().__init__()
+
+        X = np.array(pd.read_csv(".\star_classification.csv"))
+
+        temp = np.concatenate((X[:, 2:8], X[:, 10:11], X[:, 14:17]), axis=1)
+
+        self.X = np.array(temp).astype(np.float32)
+        X_std = stand(self.X)
+        self.X = torch.FloatTensor(X_std)
+
+        self.Y = np.array(X[:, 1]).astype(np.float32)
+        plt.hist(self.Y, bins=20)
+        plt.show()
+        Y_std = stand(self.Y)
+        self.Y = torch.FloatTensor(Y_std)
+
+        self.X_classes = np.array(X[:, 13:14])
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        return np.array(self.X[idx]), np.expand_dims(self.Y[idx], axis=-1)
+
+
+dataset_full = Dataset()
+train_test_split = int(len(dataset_full) * TRAIN_TEST_SPLIT)
+dataset_train, dataset_test = torch.utils.data.random_split(
+    dataset_full,
+    [train_test_split, len(dataset_full) - train_test_split],
+    generator=torch.Generator().manual_seed(0)
+)
+
+dataloader_train = torch.utils.data.DataLoader(
+    dataset=dataset_train,
+    batch_size=BATCH_SIZE,
+    shuffle=True
+)
+
+dataloader_test = torch.utils.data.DataLoader(
+    dataset=dataset_test,
+    batch_size=BATCH_SIZE,
+    shuffle=False
+)
+
+
+class BatchNorm(torch.nn.Module):
+    def __init__(self, in_features: int):
+        super().__init__()
+        self.x = None
+        self.meanvar = None
+        self.variance = None
+        self.eps = 1e-8
+        self.gamma = torch.nn.Parameter(torch.rand(size=(in_features,)))
+        self.beta = torch.nn.Parameter(torch.zeros(size=(in_features,)))
+
+
+    def forward(self, x):
+        self.x = x
+        self.meanvar = torch.mean(self.x, axis=0)
+        self.variance = torch.var(self.x, axis=0)
+        norm = ( self.x - self.meanvar ) / ( torch.sqrt(self.variance + self.eps) )
+        self.output = self.gamma * norm + self.beta
+        return self.output
+
+
+
+class Model(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layers = torch.nn.Sequential(
+            torch.nn.Linear(in_features=10, out_features=24),
+            torch.nn.ReLU(),
+            BatchNorm(in_features=24),
+            torch.nn.Linear(in_features=24, out_features=16),
+            torch.nn.ReLU(),
+            BatchNorm(in_features=16),
+            torch.nn.Linear(in_features=16, out_features=1)
+        )
+
+
+    def forward(self, x):
+        y_prim = self.layers.forward(x)
+        return y_prim
+
+
+class LossHuber(torch.nn.Module):
+    def __init__(self, delta):
+        super().__init__()
+        self.delta = delta
+
+    def forward(self, y_prim, y):
+        loss = torch.mean(
+            self.delta**2 * (torch.sqrt(1 + ((y - y_prim)/self.delta)**2) - 1.0)
+        )
+        return loss
+
+model = Model()
+optimizer = torch.optim.Adam(
+    model.parameters(),
+    lr=LEARNING_RATE
+)
+loss_fn = torch.nn.MSELoss()
+
+loss_plot_train = []
+loss_plot_test = []
+r2_plot_train = []
+r2_plot_test = []
+
+for epoch in range(1, 110):
+
+    for dataloader in [dataloader_train, dataloader_test]:
+        losses = []
+        r2s = []
+        for x, y in dataloader:
+
+            y_prim = model.forward(x)
+            loss = loss_fn.forward(y_prim, y)
+
+            np_loss = loss.data.numpy()
+            losses.append(np_loss)
+
+            np_y = y.data.numpy()
+            np_y_prim = y_prim.data.numpy()
+
+            r2 = 1 - (np.sum((np_y_prim - np_y)**2) / np.sum((np.mean(np_y) - np_y)**2))
+            r2s.append(r2)
+
+            if dataloader == dataloader_train:
+                loss.backward()
+                optimizer.step()
+                optimizer.zero_grad()
+
+        if dataloader == dataloader_train:
+            loss_plot_train.append(np.mean(losses))
+            r2_plot_train.append(np.mean(r2s))
+        else:
+            loss_plot_test.append(np.mean(losses))
+            r2_plot_test.append(np.mean(r2s))
+
+
+    print(f'epoch: {epoch} loss_train: {loss_plot_train[-1]} loss_test: {loss_plot_test[-1]} R2_train: {r2_plot_train[-1]} R2_test: {r2_plot_test[-1]}')
+
+    if epoch % 10 == 0:
+        fig, axes = plt.subplots(nrows=2, ncols=1)
+        fig.tight_layout(pad=5)
+        ax1 = axes[0]
+        ax1.plot(loss_plot_train, 'r-', label='train')
+        ax2 = ax1.twinx()
+        ax2.plot(loss_plot_test, 'c-', label='test')
+        ax1.legend()
+        ax2.legend(loc='upper left')
+        ax1.set_xlabel("Epoch")
+        ax1.set_ylabel("Loss")
+
+        ax1 = axes[1]
+        ax1.plot(r2_plot_train, 'r-', label='train')
+        ax2 = ax1.twinx()
+        ax2.plot(r2_plot_test, 'c-', label='test')
+        ax1.legend()
+        ax2.legend(loc='upper left')
+        ax1.set_xlabel("Epoch")
+        ax1.set_ylabel("r2 score")
+        plt.show()
